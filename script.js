a8a17886593b9639628b20d7a60aadfe7146e21f
Nimra6 <143000020+Nimra6@users.noreply.github.com>
2023-10-01 15:10:52 +0100

    Add files via upload
    i have done my assignment

---
+++ script.js
@@ -0,0 +1,31 @@
+// exercise 1.1
+
+var result = 4 + 10;
+
+console.log("The result of 4 + 10 is: " + result);
+
+console.log("Hello, My name is Nimra Yaqoob");
+// exercise 1.1
+
+
+  // exercise 1.3
+
+  console.log("hello world!");
+
+  // Chapter project
+
+
+  /*  This is a multiple-line comment
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ipsum?
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ipsum?
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ipsum?
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ipsum?
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, ipsum?
+  My name is Nimra yaqoob*/
+
+console.log("Nimra ghouri"); // Output my name to the console
+
+// Commenting out the console message
+// console.log("Nimra ghouri");
+
+// Chapter project
